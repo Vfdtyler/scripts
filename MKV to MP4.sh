d8c5f88ee4a471bd5624e63ed029b109c0978e03
Tyler Evans <39452745+Vfdtyler@users.noreply.github.com>
2022-07-12 21:56:14 -0400

    Add files via upload

---
+++ MKV to MP4.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+read -r -p "Delete MKV files? [y/N] " response
+for i in *.mkv
+do
+	convert-video "$i"
+	if
+		ffprobe -v error -select_streams v:0 -show_entries stream=codec_tag_string -of default=noprint_wrappers=1:nokey=1 "${i%.*}.mp4" | grep hev1 > /dev/null
+	then
+		mv "${i%.*}.mp4" "${i%.*} hev1.mp4"
+		ffmpeg -i "${i%.*} hev1.mp4" -c:v copy -tag:v hvc1 -c:a copy -c:s copy -map 0 "${i%.*}.mp4"
+		rm "${i%.*} hev1.mp4"
+	fi
+	if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+	then
+		rm "$i"
+	fi
+done
