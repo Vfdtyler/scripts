2e8787dc64bc5bd8c4d0d33d7d5e05fd59364d1f
Tyler Evans <39452745+Vfdtyler@users.noreply.github.com>
2024-02-16 00:12:57 -0500

    Add files via upload

--- FLAC to MP3.sh
+++ FLAC to MP3.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
-read -r -p "Delete FLAC files? [y/N] " response
+# Accept folder as input, if none, use current working directory
+folder=${1:-$(pwd)}
 
-for i in *.flac
+# Ask user if the original flac files should be deleted
+read -p "Do you want to delete the original FLAC files after conversion? (y/n): " delete_original
+
+cd "$folder"
+
+for file in "$folder"/*.flac
 do
-	ffmpeg -i "$i" -q:a 0 -ar 44100 "${i%.*}.mp3"
-if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
-then
-	rm "$i"
-fi
+    # Extract filename without extension
+	filename=$(basename "$file" .flac)
+	
+	# Convert to MP3
+	ffmpeg -i "$file" -q:a 0 -ar 44100 "$filename.mp3"
+	
+	# Delete original FLAC file if user confirms
+    if [ "$delete_original" == "y" ]
+	then
+		rm "$file"
+	fi
 done
